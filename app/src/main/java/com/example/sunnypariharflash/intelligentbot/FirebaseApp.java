fcc6bb9da70a53831617f77ae09c7f96cf9ce6be
sunnyp123 <sunnyparihar35@gmail.com>
2018-10-13 19:17:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sunnypariharflash/intelligentbot/FirebaseApp.java
@@ -0,0 +1,11 @@
+package com.example.sunnypariharflash.intelligentbot;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+public class FirebaseApp extends android.app.Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+    }
+}
